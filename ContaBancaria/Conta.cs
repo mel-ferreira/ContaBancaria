9f0707f23528d7cdaffab6108e614f7c452ffd21
mel-ferreira <melissamouraf@gmail.com>
2025-07-27 13:35:46 -0300

    :tada: [FIRST] Exercício resolvido - ContaBancaria

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+        public int Numero { get; private set; }
+        public string Nome;
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+        public Conta(int conta, string nome, double saldo) : this(conta, nome)
+        {
+            Saldo = saldo;
+        }
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+        public void Saque(double quantia)
+        {
+            Saldo -= (quantia + 5.00);
+        }
+
+        public override string ToString()
+        {
+           return "Conta: " + Numero + ", " + Nome + ", seu saldo é: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.Threading.Channels;
+
+namespace ContaBancaria {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem vindo!");
+            Console.WriteLine("Entre com os dados para abertura de conta");
+
+            Conta conta;
+
+            Console.Write("Número da conta: ");
+            int Numeroconta = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine()!;
+
+            Console.Write ("Deseja fazer um depósito inicial? s/n");
+            string perguntaDepositoInicial = Console.ReadLine()!;
+
+            if (perguntaDepositoInicial == "s" || perguntaDepositoInicial == "S")
+            {
+                Console.WriteLine("Insira um valor: ");
+                double depositoInicial = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+                conta = new (Numeroconta, nome, depositoInicial);
+                Console.WriteLine($"Cadastro realizado! Segue os dados: {conta}");
+            }
+            else
+            {
+                conta = new Conta(Numeroconta, nome);
+                Console.WriteLine($"Cadastro realizado! Segue os dados: {conta}");
+            }
+
+            Console.WriteLine("Realize um valor de depósito: ");
+            double quantiaDeposito = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            conta.Deposito(quantiaDeposito);
+
+            Console.WriteLine($"Dados atualizados: {conta}\n");
+
+            Console.WriteLine("Realize um saque: ");
+            double quantiaSaque = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            conta.Saque(quantiaSaque);
+
+            Console.WriteLine($"Dados atualizados: {conta}\n");
+
+        }
+    }
+}
